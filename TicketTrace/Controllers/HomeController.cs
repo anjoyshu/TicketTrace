5b727ceeb83e66edb3859be334eaf190c36b5ec3
anjoyshu <anjoyshu@gmail.com>
2021-10-14 00:04:04 +0800

    1.新增身分登入頁面 2.新增表單建立頁面

--- TicketTrace/Controllers/HomeController.cs
+++ TicketTrace/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using TicketTrace.Models;
 
 namespace TicketTrace.Controllers
 {
@@ -11,6 +9,49 @@ namespace TicketTrace.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Ticket()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (TicketEntities db = new TicketEntities())
+                {
+                    var obj = db.User.Where(a => a.Account.Equals(objUser.Account) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.UID.ToString();
+                        Session["UserName"] = obj.Name.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
 
         public ActionResult About()
---
+++ TicketTrace/Controllers/TicketMainsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TicketTrace.Models;
+
+namespace TicketTrace.Controllers
+{
+    public class TicketMainsController : Controller
+    {
+        private TicketEntities db = new TicketEntities();
+
+        // GET: TicketMains
+        public ActionResult Index()
+        {
+            return View(db.TicketMain.ToList());
+        }
+
+        // GET: TicketMains/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketMain ticketMain = db.TicketMain.Find(id);
+            if (ticketMain == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketMain);
+        }
+
+        // GET: TicketMains/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TicketMains/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TID,FID,UID,Summary,Description,Severity,Priority,CreateTime")] TicketMain ticketMain)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TicketMain.Add(ticketMain);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ticketMain);
+        }
+
+        // GET: TicketMains/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketMain ticketMain = db.TicketMain.Find(id);
+            if (ticketMain == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketMain);
+        }
+
+        // POST: TicketMains/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TID,FID,UID,Summary,Description,Severity,Priority,CreateTime")] TicketMain ticketMain)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticketMain).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ticketMain);
+        }
+
+        // GET: TicketMains/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TicketMain ticketMain = db.TicketMain.Find(id);
+            if (ticketMain == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticketMain);
+        }
+
+        // POST: TicketMains/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TicketMain ticketMain = db.TicketMain.Find(id);
+            db.TicketMain.Remove(ticketMain);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
